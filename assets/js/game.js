c25c879e274af2f9cbe46effce45b7be153fe81c
Michael Park <tjp4ca@gmail.com>
2022-05-27 22:35:18 -0400

    first functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,33 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+// more examples
+var playerName = "Tony the Robot";
+
+//Tony the Robot is ready for battle!
+console.log("Tony the Robot" + "is ready for battle!");
+
+//Tony the Robot is ready for battle!
+console.log(playerName + "is ready for battle!");
+
+//"Your robot, Tony the Robot, has won!"
+console.log("Your robot, " + playerName + ", has won!");
+
+
+
+// this creates a function named "fight"
+
+// funcion fight() {
+//     window.alert("The fight has begun");
+// }
+
+// fight();
+
+// window.alert("This is an alert! JavaScript is running!");
